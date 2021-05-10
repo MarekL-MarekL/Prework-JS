b29255fb2f6418cf8b7c7a0377443f1601d0b452
markandmark3 <markandmark3@gmail.com>
2021-05-10 22:20:51 +0200

    32-js-wieksza-liczba

---
+++ zadanie30131/js/main.js
@@ -0,0 +1,11 @@
+let num1=2;
+let num2=5;
+
+
+    if (num1>num2) {console.log(num1)
+     }
+    // else if (num1===num2) {console.log('liczby sa rowne')
+    // }
+    else {console.log(num2)}
+
+
