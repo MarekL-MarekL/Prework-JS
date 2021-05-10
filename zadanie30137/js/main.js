9acafab3760dae2e81dceff7a2c3ed135a3c8af0
markandmark3 <markandmark3@gmail.com>
2021-05-11 01:35:44 +0200

    38-js-dzialania

---
+++ zadanie30137/js/main.js
@@ -0,0 +1,11 @@
+
+function sprawdz(num1,num2){
+    if (num1-num2>=0) {
+    console.log('Wynik dodawania wynosi '+(num1+num2));
+    console.log('Wynik odejmowania wynosi '+(num1-num2));
+    console.log('Wynik mnożenia wynosi '+(num1*num2));
+    }
+    else{console.log("Wynik jest nieprawidlowy");
+    }  
+    }
+sprawdz(3,30);
