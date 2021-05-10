3a5c2f767bfc0af9b59eaa7fc25a974e24d8c715
markandmark3 <markandmark3@gmail.com>
2021-05-11 00:19:23 +0200

    35-js-sprawdzenie-liczby

---
+++ zadanie30134/js/main.js
@@ -0,0 +1,10 @@
+let n=99;
+
+function check(){
+if ((n>=100)&&(n<=200)) {
+    console.log("Liczba znajduje się w przedziale");
+}
+else{ console.log("Liczba nie znajduje się w przedziale");
+}
+}
+check(n);
